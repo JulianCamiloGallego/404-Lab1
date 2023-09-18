a1bd023d043d9a771bb6bbb38abbdcb9e1452b39
Julian Gallego Franco <gallegof@cs.ualberta.ca>
2023-09-18 09:39:19 -0600

    Updating path

--- myenv/script.py
+++ myenv/script.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.get(
-    'https://github.com/JulianCamiloGallego/404-Lab1/blob/main/script.py?raw=true')
+    'https://github.com/JulianCamiloGallego/404-Lab1/blob/main/myenv/script.py?raw=true')
 
 if response.status_code == 200:
     print(response.text)
